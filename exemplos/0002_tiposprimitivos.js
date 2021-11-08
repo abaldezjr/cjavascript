decb3b9aac9eae722684ee7dedf7e7585b8a32ee
Baldez Jr <abaldezjr@gmail.com>
2021-11-08 16:03:03 -0300

    tiposprimitivos

---
+++ exemplos/0002_tiposprimitivos.js
@@ -0,0 +1,32 @@
+// tipos primitivos - number, string, boolean, symbol, object, function
+// apesar de primitivos existem funções neles acessando sua referencia
+//number
+console.log(typeof 1);
+
+//string
+console.log(typeof '1');
+console.log(typeof "1");
+console.log(typeof 'true');
+console.log(typeof "true");
+
+//boolean
+console.log(typeof true);
+
+//symbol
+console.log(typeof Symbol("iterator"));
+
+//object
+console.log(typeof null);
+console.log(typeof {"nome": "Alexandre"})
+console.log(typeof [1,2,3])
+console.log(typeof /[a-zA-Z_$]/)
+console.log(typeof new Date())
+
+//function
+console.log(typeof function soma(a,b){return a + b;});
+
+//exemplos
+console.log((1000).toFixed(2))
+console.log('true'.length)
+console.log((true).toString())
+
