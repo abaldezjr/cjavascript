773b3bbc3e60b3f9bfd334f81ce8b2e9d63d82a5
Baldez Jr <abaldezjr@gmail.com>
2021-11-09 17:09:15 -0300

    grupo de funções para operações de strings

--- exemplos/0004_string.js
+++ exemplos/0004_string.js
@@ -31,11 +31,11 @@ let resource = "users";
 
 //interpolação forçada
 let url = "https://"+host+":"+port+"/"+resource;
-console.log(url);
+//console.log(url);
 
 //com crase e ${}
 let url2 = `https://${host}:${port}/${resource}`;
-console.log(url2);
+//console.log(url2);
 
 let mesesDoAno = "0 - Jan" + 
 "1 - Fev"+
@@ -49,7 +49,7 @@ let mesesDoAno = "0 - Jan" +
 "9 - Out"+
 "10 - Nov"+
 "11 - Dez";
-console.log(mesesDoAno);
+//console.log(mesesDoAno);
 
 let mesesDoAno2 = "0 - Jan\
 1 - Fev\
@@ -63,7 +63,7 @@ let mesesDoAno2 = "0 - Jan\
 9 - Out\
 10 - Nov\
 11 - Dez";
-console.log(mesesDoAno2);
+//console.log(mesesDoAno2);
 
 let mesesDoAno3 = "0 - Jan\n\
 1 - Fev \n\
@@ -77,7 +77,7 @@ let mesesDoAno3 = "0 - Jan\n\
 9 - Out\n\
 10 - Nov\n\
 11 - Dez\n";
-console.log(mesesDoAno3);
+//console.log(mesesDoAno3);
 
 
 let mesesDoAno4 = `0 - Jan
@@ -92,12 +92,64 @@ let mesesDoAno4 = `0 - Jan
 9 - Out
 10 - Nov
 11 - Dez`;
-console.log(mesesDoAno4);
+//console.log(mesesDoAno4);
+
+//GRUPO DE FUNÇÕES 1
+//console.log('javascript'.length);
+//console.log('PHP'.indexOf('P'));
+//console.log('PHP'.lastIndexOf('P'));
+//console.log('cobol'.toUpperCase());
+//console.log('algol'.toLowerCase());
+
+//GRUPO DE FUNÇÕES 2
+//console.log('javascript'.charAt(1));
+//console.log('javascript'.charCodeAt(1));
+//console.log(String.fromCharCode(97));
+
+//GRUPO DE FUNÇÕES 3
+//console.log('javascript'.includes('java'));
+//console.log('ruby'.startsWith('r'));
+//console.log('Erlang'.endsWith('lang'));
 
 
+//GRUPO DE FUNÇÕES 4
+//console.log('a'.localeCompare('b'));
+//console.log('a'.localeCompare('a'));
+//console.log('b'.localeCompare('a'));
 
+//console.log('a'.charCodeAt(0));
+//console.log('b'.charCodeAt(0));
+//console.log('á'.charCodeAt(0));
 
+//console.log('a' < 'b');
+//console.log('á' < 'b');
 
+//console.log('a'.localeCompare('b'));
+//console.log('á'.localeCompare('b'));
+
+//GRUPO DE FUNÇÕES 5
+//console.log("c++".match(/\+/g));
+//console.log("Java".search(/a/));//encontrei o a na posição 1
+//console.log("javascript".replace('Java', 'Ecma'));
+//console.log("javascript".replace(/a/g, 4));
+
+//GRUPO DE FUNÇÕES 6
+//console.log('javascript'.slice(0, 4));
+//console.log('javascript'.slice(4));
+//console.log('javascript'.slice(0, -6));
+//console.log('c,java,javascript,ruby'.split(','));
+//console.log('javascript'.substring(0, 4));
+//console.log('javascript'.substring(4, 0));
+//console.log('javascript'.substring(4));
+
+//GRUPO DE FUNÇÕES 7
+//console.log('java'.concat('script'));
+//console.log('script'.padStart(10, 'java'));
+//console.log('c'.padEnd(3,'+'));
+//console.log('c'.concat('+'.repeat(2)));
+//console.log(' Self '.trim());
+//console.log(' scheme '.trimLeft());
+//console.log('perl '.trimRight());
 
 
 
