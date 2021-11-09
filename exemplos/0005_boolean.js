71f3b63285316e32313a2c14689af494d49caf4c
Baldez Jr <abaldezjr@gmail.com>
2021-11-09 17:29:14 -0300

    booleanos

---
+++ exemplos/0005_boolean.js
@@ -0,0 +1,61 @@
+//console.log(typeof true);
+//console.log(typeof false);
+//console.log(typeof new Boolean(true));
+//console.log(typeof new Boolean(false));
+
+//let condition = true;
+
+//if(condition){
+//    console.log('É verdadeiro!');
+//}else {
+//    console.log('É falso!');
+//}
+
+//let condition2 = false;
+
+//if(condition2){
+//    console.log('É verdadeiro!');
+//}else {
+//    console.log('É falso!');
+//}
+
+//COERSÃO DE TIPO
+//let condition3 = new Boolean(true);
+
+//if(condition3){
+//   console.log('É verdadeiro!');
+//}else {
+//    console.log('É falso!');
+//}
+
+//let condition4 = new Boolean(false);
+
+//if(condition4){
+//    console.log('É verdadeiro!');
+//}else {
+//    console.log('É falso!');
+//}
+
+//APENAS ESTES 6 VALORES SÃO EVALUADOS PARA FALSO
+
+// PARA OS NÚMEROS 0 É FALSO E NOT A NUMBER É FALSO, O RESTO É VERDADEIRO
+//console.log(!!0);
+//console.log(!!NaN);
+
+// PARA STRING SÓ STRING VAZIA É FALSO
+//console.log(!!"");
+
+
+//console.log(!!false);
+//console.log(!!undefined);
+//console.log(!!null);
+
+//--VALORES VERDADEIROS EXEMPLOS--
+//console.log(!!-10);
+//console.log(!!'Javascript');
+//console.log(!!{});
+//console.log(!!/Javascript/);
+//console.log(!!new Date());
+//console.log(!!function (){});
+
+
