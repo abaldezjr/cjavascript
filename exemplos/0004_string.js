8c940a5e5ece126bd0553c28e8dd1bafb341d55d
Baldez Jr <abaldezjr@gmail.com>
2021-11-08 21:06:31 -0300

    declaracao e templates

---
+++ exemplos/0004_string.js
@@ -0,0 +1,103 @@
+
+//console.log(typeof 'javascript');
+//console.log(typeof "javascript");
+//console.log(typeof `javascript`);
+
+//console.log('javascript' == 'javascript');
+//console.log('javascript' === 'javascript');
+
+//console.log('javascript' == "javascript");
+//console.log('javascript' === "javascript");
+
+//console.log('javascript' == `javascript`);
+//console.log('javascript' === `javascript`);
+
+//console.log("javascript" == `javascript`);
+//console.log("javascript" === `javascript`);
+
+//let s1 = new String('javascript');
+//let s2 = new String("javascript");
+//let s3 = new String(`javascript`);
+
+//console.log(s1);
+//console.log(s2);
+//console.log(s3);
+
+//template literal
+
+let host = "localhost";
+let port = "3000";
+let resource = "users";
+
+//interpolação forçada
+let url = "https://"+host+":"+port+"/"+resource;
+console.log(url);
+
+//com crase e ${}
+let url2 = `https://${host}:${port}/${resource}`;
+console.log(url2);
+
+let mesesDoAno = "0 - Jan" + 
+"1 - Fev"+
+"2 - Mar"+
+"3 - Abr"+
+"4 - Mai"+
+"5 - Jun"+
+"6 - Jul"+
+"7 - Ago"+
+"8 - Set"+
+"9 - Out"+
+"10 - Nov"+
+"11 - Dez";
+console.log(mesesDoAno);
+
+let mesesDoAno2 = "0 - Jan\
+1 - Fev\
+2 - Mar\
+3 - Abr\
+4 - Mai\
+5 - Jun\
+6 - Jul\
+7 - Ago\
+8 - Set\
+9 - Out\
+10 - Nov\
+11 - Dez";
+console.log(mesesDoAno2);
+
+let mesesDoAno3 = "0 - Jan\n\
+1 - Fev \n\
+2 - Mar\n\
+3 - Abr\n\
+4 - Mai\n\
+5 - Jun\n\
+6 - Jul\n\
+7 - Ago\n\
+8 - Set\n\
+9 - Out\n\
+10 - Nov\n\
+11 - Dez\n";
+console.log(mesesDoAno3);
+
+
+let mesesDoAno4 = `0 - Jan
+1 - Fev
+2 - Mar
+3 - Abr
+4 - Mai
+5 - Jun
+6 - Jul
+7 - Ago
+8 - Set
+9 - Out
+10 - Nov
+11 - Dez`;
+console.log(mesesDoAno4);
+
+
+
+
+
+
+
+
