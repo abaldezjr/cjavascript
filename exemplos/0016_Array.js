f22d6ec84c43b95daea07c429e03eba0eff23397
Baldez Jr <abaldezjr@gmail.com>
2021-11-11 22:36:44 -0300

    Array

---
+++ exemplos/0016_Array.js
@@ -0,0 +1,76 @@
+//ARRAY
+// const languages = ['Python', 'C', 'Java'];
+// console.log(typeof languages);
+// console.log(languages instanceof Array);
+// console.log(languages);
+
+// const languages2 = new Array('Python', 'C', 'Java');
+// console.log(typeof languages2);
+// console.log(languages2 instanceof Array);
+// console.log(languages2);
+
+//exemplo 2
+//pegadinha da linguagem
+// o segundo array tem 3 posições e não o elemento 3
+// const numbers = new Array(1, 2, 3);
+// console.log(numbers);
+
+// const numbers2 = new Array(3);
+// console.log(numbers2);
+
+//exemplo 3
+// const languages = [];
+// languages[0] = 'Python';
+// languages[1] = 'C';
+// languages[2] = 'Java';
+// console.log(languages);
+// console.log(languages.length);
+// delete languages[1];
+// console.log(languages);
+// console.log(languages.length);
+
+//exemplo 4
+// const languages = [];
+// languages[0] = 'Python';
+// languages[10] = 'C';
+// languages[20] = 'Java';
+// console.log(languages);
+// console.log(languages.length);
+// delete languages[10];
+// console.log(languages);
+// console.log(languages.length);
+
+//ARRAY MUTATOR API
+// reverse - inverte a ordem dos elementos
+// fill - preenche elementos de acordo com a posição de inicio e fim
+
+// const languages = ['Python', 'C', 'Java'];
+// console.log(languages);
+// // push - adiciona um elemento ao final
+// console.log(languages.push('Ruby'));
+// console.log(languages);
+// // pop - remove o elemento do final
+// console.log(languages.pop());
+// console.log(languages);
+// // unshift = adiciona um elemento ao inicio
+// console.log(languages.unshift('Go'));
+// console.log(languages);
+// // shift - remove o elemento do inicio
+// console.log(languages.shift());
+// console.log(languages);
+// // splice - remove, substitui ou adiciona um ou mais elementos em uma determinada posição
+// console.log(languages.splice(1, 2));
+// console.log(languages);
+// console.log(languages.splice(1, 0, 'C++', 'C#'));
+// console.log(languages);
+// console.log(languages.splice(1, 2, 'C','Java'));
+// console.log(languages);
+// // sort - ordena os elementos de acordo com a função de ordenação
+
+
+
+
+
+
+
+
